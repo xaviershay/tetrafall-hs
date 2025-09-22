1035ee0cdebdb260e8637e8725c9b910846e908f
Xavier Shay <contact@xaviershay.com>
2025-09-22 13:15:17 +1000

    get a tick going

--- app/Main.hs
+++ app/Main.hs
@@ -8,36 +8,39 @@ import qualified Data.Vector as V
 
 import Lens.Micro.Platform
 
-import Control.Monad (void)
-
 import Brick
+import Brick.BChan (newBChan, writeBChan)
+import Brick.Widgets.Core (padLeft, Padding(Max))
 import Brick.Widgets.Border (border)
 import Brick.Widgets.Center (hCenter, vCenter)
+import Control.Monad (forever, void)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.State.Class (MonadState, modify, get)
+import Control.Concurrent (threadDelay, forkIO)
 import qualified Graphics.Vty as V
 import Graphics.Vty.CrossPlatform (mkVty)
 import Graphics.Vty.Config (VtyUserConfig(..), defaultConfig)
 import Graphics.Vty.Attributes.Color (ColorMode(..))
 
-data AppState = AppState
+data Tick = Tick
+
+data Game = Game
   { _grid :: Grid
+  , _score :: Int
   }
-makeLenses ''AppState
+makeLenses ''Game
 
---ui :: Widget ()
---ui =
---    border $
---    hLimit 10 $
---    vLimit 22 $
---    (foldl (<+>) (str "") (map (\i -> withDefAttr (redAttr (i * 2)) $ str (show i)) [0..10]))
+playfield :: Game -> Widget ()
+playfield game =
+    let
+      g = view grid game
+      s = view score game
+    in
 
-playfield :: Grid -> Widget ()
-playfield grid =
     hCenter $
     vCenter $
     (border $
-    --hLimit 10 $
-    --vLimit 22 $
-    foldl (<=>) (str "") (V.map (\row -> foldl (<+>) (str "") (V.map formatCell row)) (view contents (doubleGrid grid)))) <+> (border $ str "12345")
+    foldl (<=>) (str "") (V.map (\row -> foldl (<+>) (str "") (V.map formatCell row)) (view contents (doubleGrid g)))) <+> ( border $ hLimit 6 $ padLeft Max $ str (show s))
 
 formatCell Empty = str " "
 formatCell _ = str "█"
@@ -53,9 +56,18 @@ doubleGrid grid = Grid
     doubleRow row = [doubledRow, doubledRow]
       where doubledRow = V.fromList $ concatMap (\cell -> [cell, cell]) (V.toList row)
 
-appEvent :: BrickEvent () e -> EventM () AppState ()
+step :: Game -> Game
+step = over score ((+) 1)
+
+appEvent :: BrickEvent () Tick -> EventM () Game ()
 appEvent (VtyEvent (V.EvKey V.KEsc [])) = halt
 appEvent (VtyEvent (V.EvKey (V.KChar 'q') [])) = halt
+appEvent (AppEvent Tick) = do
+    x <- get
+    put x
+    modify step
+    return ()
+
 appEvent _ = return ()
 
 redAttr :: Int -> AttrName
@@ -65,9 +77,9 @@ attributes :: AttrMap
 attributes =  attrMap (V.green `on` V.black) $
     map (\i -> (redAttr i, bg (V.rgbColor i 0 0))) [0..255]
 
-app :: App AppState e ()
+app :: App Game Tick ()
 app =
-    App { appDraw = \s -> [playfield (_grid s)]
+    App { appDraw = \s -> [playfield s]
         , appHandleEvent = appEvent
         , appStartEvent = return ()
         , appAttrMap = const attributes
@@ -76,10 +88,15 @@ app =
 
 main :: IO ()
 main = do
+    chan <- newBChan 10
+    forkIO $ forever $ do
+        writeBChan chan Tick
+        threadDelay 100000 -- decides how fast your game moves
     let buildVty = mkVty $ defaultConfig { configPreferredColorMode = Just FullColor }
     initialVty <- buildVty
-    void $ customMain initialVty buildVty Nothing app defaultAppState
+    void $ customMain initialVty buildVty (Just chan) app defaultGame
 
-defaultAppState = AppState
+defaultGame = Game
   { _grid = setAt (5, 5) Garbage $ setAt (5, 10) (TetrominoCell T) $ setAt (6, 10) (TetrominoCell T) $ mkGrid 10 22
+  , _score = 0
   }
